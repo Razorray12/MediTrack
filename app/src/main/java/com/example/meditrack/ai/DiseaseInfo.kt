bd6f06fe3fc9190a15180dab98b95f8016236914
Denis <den.potemkin2015@mail.ru>
2025-01-15 20:25:13 +0300

    deletedFiles

--- app/src/main/java/com/example/meditrack/ai/DiseaseInfo.kt
+++
@@ -1,7 +0,0 @@
-package com.example.meditrack.ai
-
-data class DiseaseInfo(
-    val drugName: String?,
-    val symptoms: String?,
-    val description: String?
-)
--- app/src/main/java/com/example/meditrack/ai/PossibleDiseasesInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.meditrack.ai
-
-data class PossibleDiseasesInfo(
-    val diseaseName: String?,
-    val description: String?
-)
